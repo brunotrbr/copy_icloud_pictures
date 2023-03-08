3c496f6fbda451a699b9e03db1635bb2f6a13276
Bruno Trindade Bragança <bbmendes@gmail.com>
2023-03-08 07:20:10 -0300

    Changes
    Changed 01 to receive destiny and year as args
    
    Changed 02 to test if path exists. If not exists, exit. Test if file exists. If not exists, copy. If exists do nothing.

--- 02_copy_pictures.ps1
+++ 02_copy_pictures.ps1
@@ -1,7 +1,7 @@
 ﻿<#
 Author: Bruno Trindade Bragança
 Github: https://github.com/brunotrbr
-Date: 23/02/2023
+Date: 08/03/2023
 #>
 
 $icloud_path = Read-Host "Type iCloud path (origin) without last slash (\)"
@@ -10,13 +10,12 @@ $icloud_path = $icloud_path + "\"
 $year = (Get-Date).Year
 
 $destiny_path = Read-Host "Type destiny path without last slash (\)"
-$log_file = $destiny_path + "\log_" + (Get-Date -Format yyyy_MM_dd) + ".txt"
+$destiny_path = $destiny_path + "\"
 
-$destiny_path = $destiny_path + "\" + $year + "\"
+$log_file = $destiny_path + "log_" + (Get-Date -Format yyyy_MM_dd) + ".txt"
 
 Write-host
 Write-host "Starting copy files from" $icloud_path "to" $destiny_path
-
 
 Write-host
 Write-Host "Creating log file"
@@ -30,28 +29,37 @@ Write-Host "Copying..."
 Write-host "Copying files from" $icloud_path "to" $destiny_path  *>> $log_file
 Write-Host "" *>> $log_file
 
-
 $files = Get-ChildItem -Path $icloud_path 2>> $log_file
 
-$exclude = Get-ChildItem -recurse $destiny_path 2>> $log_file
-
-
 foreach ($file in $files)
-{
-    $current_file = $icloud_path + $file
-    
-    $creation_date = (Get-ChildItem -File $current_file).CreationTime 2>> $log_file
-    
-    $month = $creation_date.Month
-    
-    if ($month -lt 10)
+{    
+    $creation_date = (Get-ChildItem -File $file.FullName).CreationTime 2>> $log_file
+
+    $file_month = $creation_date.Month
+    $file_year = $creation_date.Year
+    $file_year = 2022
+
+    if ($file_month -lt 10)
     {
-        $month = "0" + $month
+        $file_month = "0" + $file_month
+    }
+    
+    $path_to_save = $destiny_path + $file_year
+
+    if ( -not (Test-Path -Path $path_to_save)) {
+        Write-Host "$path_to_save not found." *>> $log_file
+        Write-Host "End"
+        Exit
     }
 
-    $path_to_save = $destiny_path + $month
-    Copy-Item -Path $current_file -Destination $path_to_save -Exclude $exclude -Verbose *>> $log_file
-    Write-Host "" *>> $log_file
+    $source = $icloud_path + $file.Name
+    $destination = $path_to_save + "\" + $file_month
+    $full_path_file = $destination + "\" + $file.Name
+
+    if ( -not (Test-Path -Path $full_path_file -PathType Leaf)) {
+        Copy-Item -Path $source -Destination $destination -Verbose *>> $log_file
+        Write-Host "" *>> $log_file
+    }
 }
 
 Write-Host
